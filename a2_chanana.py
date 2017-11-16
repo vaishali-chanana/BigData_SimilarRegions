634f9b9407f4013db4c401507219fb17927ce5fe
vchanana <vchanana@cs.stonybrook.edu>
2017-11-16 16:47:51 -0500

    Part 1 is done

--- a2_chanana.py
+++ a2_chanana.py
@@ -1,11 +1,66 @@
 import sys
+import io
+import numpy as np
+import zipfile
 from pyspark import SparkContext, SparkConf
+from tifffile import TiffFile
+
+def getOrthoTif(zfBytes):
+    #given a zipfile as bytes (i.e. from reading from a binary file),
+    #  return a np array of rgbx values for each pixel
+    bytesio = io.BytesIO(zfBytes)
+    zfiles = zipfile.ZipFile(bytesio, "r")
+
+    #find tif:
+    for fn in zfiles.namelist():
+        if fn[-4:] == '.tif':#found it, turn into array:
+            tif = TiffFile(io.BytesIO(zfiles.open(fn).read()))
+        return tif.asarray()
+
+def reshapeTiff(x):
+    m,n,r = x.shape
+    t = 500
+    rowRange = range(0,m,t)
+    colRange = range(0,n,t)
+    reshaped = []
+    #iCtr = 0
+    for row in rowRange:
+        for col in colRange:
+            #jCtr =0
+            reshaped.append(x[row:row+t,col:col+t,:])
+    return reshaped
+
+
+def nameForImage(x):
+    l = x[0]
+    y = np.array(x[1])
+    #print(y.shape)
+    i,m,n,r = y.shape
+    for cnt in range(i):
+        v = l+"-"+str(cnt)
+        yield(v,y[cnt])
+
+
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("BDAssi2_vchanana").setMaster("local[2]")
     sc = SparkContext(conf=conf)
 
+    #__VAISHALI__2017_11_15__Reading from files
     path_var = 'C:\\Vaishali Chanana\\SBU\\545 Big Data\\HW2\\a2_small_sample'
+    # read path of zip folder from command line arguments
+    #path var = sys.argv[1]
     rdd = sc.binaryFiles(path_var)
+    #rdd.persist()
 
-    print("Hi")
+    # 1(a)
+    newRdd = rdd.map(lambda x: (x[0].split('/')[-1:][0], x[1]))
+
+    # 1(b) ,1(c) and 1(d)
+    tiffRdd = newRdd.map(lambda x: (x[0], getOrthoTif(x[1]))).map(lambda x:(x[0], reshapeTiff(x[1]))).flatMap(lambda x: nameForImage(x))
+
+    # for printing 1(e)
+    oneRdd = tiffRdd.filter(lambda x: x[0] in {"3677454_2025190.zip-0","3677454_2025195.zip-1","3677454_2025195.zip-18","3677454_2025195.zip-19"}).map(lambda x:x[1][0][0])
+    print("\n----------------Step 1 Results----------------------\n")
+    print(oneRdd.collect())
+
